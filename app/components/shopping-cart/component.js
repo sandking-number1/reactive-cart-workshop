65e25b551f527387e8c0be4ce9e474c15675fbae
sandking-number1 <Alfred.J9412@gmail.com>
2016-04-10 11:30:33 -0700

    Changed a bunch of stuff around how the initial presenting controller handles children bindings for actions

--- app/components/shopping-cart/component.js
+++ app/components/shopping-cart/component.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
+const {
+  computed,
+} = Ember;
+
 export default Ember.Component.extend({
+  cartService: Ember.inject.service('cart'),
+  cart: computed('products', 'subtotal', () => {
+    const cart = { products: [], subtotal: 0.0 };
+    return cart;
+  }),
+
+  actions: {
+    updateProduct(product) {
+      console.log('product is: ', product);
+    },
+    removeProduct(product) {
+      console.log('product is: ', product);
+    },
+  },
 });
